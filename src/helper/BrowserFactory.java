7dea0092dc0621f34af736268f4846c6dc9230b9
Fabio Lima <f46io@icloud.com>
2019-10-04 19:37:07 +0200

    Dev Branch.”

---
+++ src/helper/BrowserFactory.java
@@ -0,0 +1,29 @@
+package helper;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserFactory {
+	
+	static WebDriver driver;
+	
+	public static WebDriver startBrowser(String browserName, String url) 
+	{
+	
+		if(browserName.equals("firefox"))
+		{
+			driver=new FirefoxDriver();
+		}
+		else if (browserName.equals("chrome")) {
+			driver=new ChromeDriver();
+		}
+	
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(url);
+		return driver;
+	}
+}
